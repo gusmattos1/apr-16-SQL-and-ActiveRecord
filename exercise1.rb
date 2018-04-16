2855b7260052f14ae5e05be27300e335a7d17b5e
gusmattos1 <gusmattos@gmail.com>
2018-04-16 14:39:20 -0400

    improved

--- exercise1.rb
+++ exercise1.rb
@@ -5,28 +5,27 @@ classe  = [
   ["Gismo", "Shadow", nil, nil],
   ["Smokey","Toast","Pacha","Mau"]
 ]
-new_classe = []
-classe.each do |array|
-  hash = {}
-  array.each_with_index do |item, index|
-    hash[index+1] = item
-  end
-  new_classe << hash
-end
-i = 1
+# new_classe = []
+# classe.each do |array|
+#   hash = {}
+#   array.each_with_index do |item, index|
+#     hash[index+1] = item
+#   end
+#   new_classe << hash
+# end
+# i = 1
 
-new_classe.each do |hash|
-  hash.each do |key, value|
-    if value == nil
-      puts " Row #{i} seat #{key} is free. Do you want to sit there? (y/n)"
+classe.each do |array|
+  array.each_with_index do |seat, index|
+    if seat == nil
+      puts " Row #{classe.index(array) + 1} seat #{index + 1} is free. Do you want to sit there? (y/n)"
       answer = gets.chomp
-        if answer.downcase == "y"
-          puts "What is your name?"
-          name = gets.chomp
-          hash[key] = name
-        end
+      if answer.downcase == "y"
+        puts "What is your name?"
+        name = gets.chomp
+        classe[classe.index(array)][index] = name
+      end
     end
   end
-  i += 1
 end
-puts new_classe
+print classe
